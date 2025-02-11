318b6b3af333f629c68b2b279f82b4c6cc8ea1d3
Sam1871 <sam1871dev@gmail.com>
2025-02-11 15:01:29 +0530

    fixed file upload path

--- middleware/multer.js
+++ middleware/multer.js
@@ -3,14 +3,15 @@ import { v4 as uuid } from "uuid";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "D:\\React Projects\\E_learning\\Server\\tmp\\my-uploada");
+    cb(null, "../tmp/my-uploada/");
   },
   filename: function (req, file, cb) {
     const id = uuid();
     const FileExt = file.originalname.split(".").pop()
     // console.log(FileExt)
     cb(null, `${id}.${FileExt}`);
-    // console.log(file)
+    // console.log(file)ls
+    
   }
 });
 
